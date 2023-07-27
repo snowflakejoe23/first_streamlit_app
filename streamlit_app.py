e949160e3b403f3c00013d3a415690e060828198
snowflakejoe23 <139471195+snowflakejoe23@users.noreply.github.com>
2023-07-27 14:21:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast Menu")
 streamlit.text(" 🥣 Omega 3 & Blueberry Oatmeal")
@@ -6,11 +11,6 @@ streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text(" 🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
 
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
